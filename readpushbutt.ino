36ddb7c7daad61721db194dc06331639f9bc13ee
SasaniP <102651746+SasaniP@users.noreply.github.com>
2022-08-24 13:33:58 +0530

    Add files via upload

---
+++ readpushbutt.ino
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void) //main starts
+{
+  DDRD = DDRD | ( 1<<4) ; //Make pin 4 of port D as a output
+  DDRB = DDRB & ~(1<<0) ; // Make pin 5 of port C as a input
+  while (1) //initialize while loop
+  {
+    if(PINB & (1<<0) ) //if PIN5 of port C is high
+    {
+      PORTD = PORTD | ( 1<<4) ; //PIN4 of port D is high
+    }
+    else //otherwise
+    {
+      PORTD = PORTD &  ~( 1<<4) ; //PIN4 of port D will remain low
+    }
+  }
+}
